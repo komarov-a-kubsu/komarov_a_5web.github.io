2f4c42979659158a071a752931c04260059c6e45
komarov-a-kubsu <113387514+komarov-a-kubsu@users.noreply.github.com>
2022-10-19 17:41:12 +0300

    Add files via upload

---
+++ calcscr.js
@@ -0,0 +1,17 @@
+function isNumeric(num) {
+    return !isNaN(num)
+}
+
+
+function click1() {
+    let f1 = document.getElementsByName("field1");
+    let f2 = document.getElementsByName("field2");
+    let r = document.getElementById("result");
+    if (isNumeric(f1[0].value) && isNumeric(f2[0].value) && f1[0].value!=null && f2[0].value!=null) {
+        r.value = Number(f1[0].value) * Number(f2[0].value);
+    }
+    else{
+        r.value = "Something went wrong"
+    }
+    return false;
+}
