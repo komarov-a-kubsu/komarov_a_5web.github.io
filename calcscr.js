a776dcb9a796b2e5c65a6a355bab5ecbf579a1d9
komarov-a-kubsu <113387514+komarov-a-kubsu@users.noreply.github.com>
2022-10-25 13:02:07 +0300

    Update calcscr.js

--- calcscr.js
+++ calcscr.js
@@ -7,7 +7,7 @@ function click1() {
     let f1 = document.getElementsByName("field1");
     let f2 = document.getElementsByName("field2");
     let r = document.getElementById("result");
-    if (isNumeric(f1[0].value) && isNumeric(f2[0].value) && f1[0].value!=null && f2[0].value!=null) {
+    if (isNumeric(f1[0].value) && isNumeric(f2[0].value) && f1[0].value!=null && f2[0].value!=null && f1[0].value>0 && f2[0].value>0) {
         r.value = Number(f1[0].value) * Number(f2[0].value);
     }
     else{
